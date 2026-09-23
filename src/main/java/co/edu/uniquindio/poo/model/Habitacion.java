440839307f0ce61aa0f60beedf8f44b5f68c2eb8
dilanrondonb-debug <dilana.rondonb@uqvirtual.edu.co>
2026-09-23 17:08:07 -0500

    atributos y contructor

--- src/main/java/co/edu/uniquindio/poo/model/Habitacion.java
+++ src/main/java/co/edu/uniquindio/poo/model/Habitacion.java
@@ -1,4 +1,6 @@
 package co.edu.uniquindio.poo.model;
 
 public class Habitacion {
+
+
 }
--- src/main/java/co/edu/uniquindio/poo/model/Hotel.java
+++ src/main/java/co/edu/uniquindio/poo/model/Hotel.java
@@ -1,4 +1,21 @@
 package co.edu.uniquindio.poo.model;
 
 public class Hotel {
+    private String nombreComercial;
+    private String nit;
+    private String direccion;
+    private String telefono;
+
+    private List<Huesped> listaHuespedes;
+    private List<Habitacion> listaHabitaciones;
+    //constructor
+    public hotel (String nombreComercial, String nit, String direccion, String telefono ) {
+    this.nombreComercial=nombreComercial;
+    this.nit=nit;
+    this.direccion=direccion;
+    this.telefono=telefono;
+    this.listaHabitaciones=new Arraylist<>();
+    this.listaHuespedes=new ArrayList<>();
+    }
+    //
 }
