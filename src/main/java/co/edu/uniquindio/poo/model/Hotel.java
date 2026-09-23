dd2e82cd2182e5b4b04af4d6c7abd433d37c2b03
dilanrondonb-debug <dilana.rondonb@uqvirtual.edu.co>
2026-09-23 18:30:58 -0500

    metodos de la clase hotel
    falta la logica de realizar y cancelar reserva

--- src/main/java/co/edu/uniquindio/poo/model/Hotel.java
+++ src/main/java/co/edu/uniquindio/poo/model/Hotel.java
@@ -17,5 +17,45 @@ public class Hotel {
     this.listaHabitaciones=new Arraylist<>();
     this.listaHuespedes=new ArrayList<>();
     }
-    //
+    //metodos
+    public boolean consultarNuevoHuespedPorTelefono(String telefono){
+        for(Huesped h: listaHuespedes){
+            if(h.getTelefono().equals(telefono)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void agregarHuesped(Huesped huesped){
+        this.listaHuespedes.add(huesped);
+    }
+    public void agregarHabitacion(Habitacion habitacion){
+        this.listaHabitaciones.add(habitacion);
+    }
+    public void realizarReserva(Reserva reserva){
+
+    }
+    public void cancelarReserva(String codigoReserva){
+
+    }
+    //getters y setters
+    public String getNombreComercial(){return nombreComercial;}
+    public void setNombreComercial(String nombreComercial){this.nombreComercial=nombreComercial;}
+
+    public String getNit(){return nit;}
+    public void setNit(String nit){this.nit=nit;}
+
+    public String getDireccion(){return direccion;}
+    public void setDireccion(String direccion){this.direccion=direccion;}
+
+    public String getTelefono(){return telefono;}
+    public void setTelefono(String telefono){this.telefono=telefono;}
+
+    public List<Habitacion> getListaHabitaciones() {
+        return listaHabitaciones;
+    }
+
+    public List<Huesped> getListaHuespedes() {
+        return listaHuespedes;
+    }
 }
